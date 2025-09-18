59d3f5f89787f7fbaccff570b335fbef8eb02e39
BruhLord <nikita.nikita55@mail.ru>
2025-09-18 13:42:38 +0600

    Add StartActivity.java

--- app/src/main/java/com/example/mathgame/StartActivity.java
+++ app/src/main/java/com/example/mathgame/StartActivity.java
@@ -1,6 +1,8 @@
 package com.example.mathgame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +17,12 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_start);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button startBtn = findViewById(R.id.startButton);
+        startBtn.setOnClickListener(v -> {
+            Intent intent = new Intent(StartActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
         });
     }
 }
